4a552cb2f88fd14348f99724f6db46064d16882b
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-15 22:11:36 -0700

    display moon

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,20 @@ function App() {
   return (
     <div>
       <Parallax pages={4}>
-        <ParallaxLayer>
+        {/* <ParallaxLayer>
           <h2>Welcome to my website</h2>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.5}>
           <h2>Web Development is fun!</h2>
-        </ParallaxLayer>
+        </ParallaxLayer> */}
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
       </Parallax>
     </div>
   );
